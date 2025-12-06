a6514e58806a3a2200a6c2fd070a143fcd0bb301
Kim Dudenhöfer <kidu2400@student.miun.se>
2025-12-06 12:39:31 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//Getting Fastify
+const fastify = require('fastify')({
+    logger: true
+});
+
+//Routes
+fastify.get('/', async(request, reply) => {
+    return ({ hello: 'world' });
+});
+
+//Connecting to server
+const start = async() => {
+    try {
+        await fastify.listen({ port: 5000, host: '0.0.0.0' });
+    } catch(err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+
+start();
